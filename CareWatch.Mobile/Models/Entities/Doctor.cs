1482eb0bcf014180d229c746197b7513e301777a
David Dzabakhidze <dzabah.david@gmail.com>
2023-12-21 14:51:27 +0200

    Added additional fields

--- CareWatch.Mobile/Models/Entities/Doctor.cs
+++ CareWatch.Mobile/Models/Entities/Doctor.cs
@@ -3,5 +3,8 @@
     public class Doctor : BaseEntity
     {
         public Contact Contact { get; set; }
+
+        public ICollection<MedicalHistory> MedicalHistories { get; set; }
+        public ICollection<EmergencyRequest> EmergencyRequests { get; set; }
     }
 }
--- CareWatch.Mobile/Models/Entities/Patient.cs
+++ CareWatch.Mobile/Models/Entities/Patient.cs
@@ -9,5 +9,8 @@ namespace CareWatch.Mobile.Models.Entities
     public class Patient : BaseEntity
     {
         public Contact Contact { get; set; }
+
+        public ICollection<MedicalHistory> MedicalHistories { get; set; }
+        public ICollection<EmergencyRequest> EmergencyRequests { get; set; }
     }
 }
